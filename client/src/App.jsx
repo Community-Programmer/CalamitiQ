8f428b91eae4093f5c66ec2fd7a509bf029c5987
Community-Programmer <sarthakpatel230204@gmail.com>
2024-08-10 16:08:39 +0530

    Add login page and some libraries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import MainLayout from "./Layout/MainLayout";
+import Login from "./pages/Auth/Login";
 import Home from "./pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
-import Community from "./pages/Community/Community";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="community" element={<Community />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </>
