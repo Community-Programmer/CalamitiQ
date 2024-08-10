d5c2dc071c75ed2ac3080a46e47141a8e3196ce5
Varun G Bhat <141301727+VarunGBhat@users.noreply.github.com>
2024-08-10 15:17:18 +0530

    Add Navbar and Footer

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            CALAMITIQ
+          </span>
+
+          <div class="flex items-center space-x-6 rtl:space-x-reverse">
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Signup
+            </button>
+          </div>
+        </div>
+      </nav>
+      <nav class="bg-gray-50 dark:bg-gray-700">
+        <div class="max-w-screen-xl px-4 py-3 mx-auto">
+          <div class="flex items-center">
+            <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <li>
+                <a
+                  href="#"
+                  class="text-gray-900 dark:text-white hover:underline"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="text-gray-900 dark:text-white hover:underline"
+                >
+                  News
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="text-gray-900 dark:text-white hover:underline"
+                >
+                  Explore
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="text-gray-900 dark:text-white hover:underline"
+                >
+                  Affected areas
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="text-gray-900 dark:text-white hover:underline"
+                >
+                  weather forecast
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
